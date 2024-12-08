3e05cf8b629ad5044d6f516d3afdc4660a9b8257
Илья <vasilijcapaev665@gmail.com>
2024-12-08 13:24:16 +0300

    Update Bot

--- u_13_3_bot_second.py
+++ u_13_3_bot_second.py
@@ -2,21 +2,19 @@ import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-api = '7704597688:AAEgiaik-tnh3YqW3_8U8qXujRYjKqqOHWY'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands='start')
 async def start(message):
-    print('Привет! Я бот продающий слона.')
-    await message.answer('Привет! Я бот продающий слона.')
-    await message.answer('Купи слона!')
-
+    print('Поступила команда start')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def start(message):
     print('Пришло:', message.text)
-    await message.answer(f'Все говорят "{message.text}" А ты купи слона!')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
